475577908e4b8daa5fa8c5fd98899baf882009e8
gekido <gekido@student.42.fr>
2026-02-12 17:12:44 +0100

    Complete cpp06: ex00, ex01, ex02 all working

---
+++ ex02/main.cpp
@@ -0,0 +1,74 @@
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <typeinfo>
+
+Base* generate(void)
+{
+    int random = rand() % 3;
+    
+    if (random == 0)
+        return new A();
+    else if (random == 1)
+        return new B();
+    else
+        return new C();
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p) != NULL)
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p) != NULL)
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p) != NULL)
+        std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+    try
+    {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        return;
+    }
+    catch (std::bad_cast &e) {}
+    
+    try
+    {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        return;
+    }
+    catch (std::bad_cast &e) {}
+    
+    try
+    {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+        return;
+    }
+    catch (std::bad_cast &e) {}
+}
+
+int main()
+{
+    srand(time(NULL));
+    
+    for (int i = 0; i < 2; i++)
+    {
+        Base* ptr = generate();
+        std::cout << "Pointer: ";
+        identify(ptr);
+        std::cout << "Reference: ";
+        identify(*ptr);
+        delete ptr;
+    }
+    
+    return (0);
+}
